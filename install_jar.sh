260f7e996441fb8f2c5bd3db36166d49afb45102
Jesse c j van Dam <jesse.vandam@wur.nl>
2015-12-02 15:48:01 +0100

    make sure that install script does not fail if cytoscape configuration can not be found

--- install_jar.sh
+++ install_jar.sh
@@ -1,1 +1,4 @@
-cp target/RDF2GraphViewer-1.0.0-SNAPSHOT.jar ~/CytoscapeConfiguration/3/apps/installed/
+if [ -d ~/CytoscapeConfiguration/3/apps/installed/ ]; then
+  echo "installing jar to Cytoscape"
+  cp target/RDF2GraphViewer-1.0.0-SNAPSHOT.jar ~/CytoscapeConfiguration/3/apps/installed/
+fi
